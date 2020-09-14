d4da8e68ffea92af935784a2c42055dfc83a9a69
Ben W <bwiedeback@gmail.com>
2020-09-14 15:32:21 -0600

    hungry planets

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,82 @@
 <template>
+  <div id="app" class="container-fluid">
+    <div class="row">
+      <div class="card col-3" v-for="planet in planets" :key="planet.name">
+        <img class="img-fluid" :src="planet.imgUrl"/>
+        <div class="card-body">
+          <h4 class="card-title"> {{ planet.name }}</h4>
+          <p class="card-text">Vespene Gas: {{planet.vespeneGas}}</p>
+          <p class="card-text">Pylons: {{planet.pylons}}</p>
+          <p class="card-text" v-if="planet.demons >= 0">Demons: {{planet.demons}} </p>
+          <p class="card-text" v-if="planet.hunger >= 0">Hunger: {{planet.hunger}} </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  mounted(){
+
+  },
+  data(){
+    return {
+      planets: [
+        {
+          name: "Venus",
+          vespeneGas: 0,
+          pylons: 0,
+          imgUrl: "https://scitechdaily.com/images/Venus-Planet.jpg"
+        },
+        {
+          name: "Mars",
+          vespeneGas: 0,
+          pylons: 0,
+          demons: 0,
+          imgUrl: "https://solarsystem.nasa.gov/internal_resources/3841"
+        },
+        {
+          name: "Jupiter",
+          vespeneGas: 0,
+          pylons: 0,
+          hunger: 0,
+          imgUrl: "https://cdn.mos.cms.futurecdn.net/iEv6pmS4gfbefs5JbwHWiJ.png"
+        },
+        {
+          name: "Saturn",
+          vespeneGas: 0,
+          pylons: 0,
+          demons: 0,
+          hunger: 0,
+          imgUrl: "https://upload.wikimedia.org/wikipedia/commons/c/c7/Saturn_during_Equinox.jpg"
+        }
+      ]
+    }
+  },
+  methods: {
+    vespeneUp(){
+      for(let key in this.planets){
+        let planet = this.planets[key]
+        planet.vespeneGas+=1
+      }
+    },
+    demonsUp(){
+      for(let key in this.planets){
+        let planet = this.planets[key]
+        planet.demons+=5
+      }
+    },
+    hungerUp(){
+      for(let key in this.planets){
+        let planet = this.planets[key]
+        planet.hunger+=10
+      }
+    }
+  }
+}
+</script>
+
 
 <style>
 #app {
